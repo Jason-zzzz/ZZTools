e036e589046045dd762466f8b03b4da1fbc7d014
apple <apple@appledeiMac.local>
2019-03-06 18:21:11 +0800

    +

--- JZZTools.podspec
+++ JZZTools.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "JZZTools"
-  s.version      = "0.1.3"
+  s.version      = "0.1.5"
   s.summary      = "登录模块"
 
   #s.description  = <<-DESC
--- ZZTools/Classes/UIImage+PrivateBundleImage.m
+++ ZZTools/Classes/UIImage+PrivateBundleImage.m
@@ -13,7 +13,12 @@
 
 + (UIImage *)imageNamed:(NSString *)imageName inPrivateBundle:(NSString *)bundleName {
     
-    NSBundle *bundle = [NSBundle privateBundleWithName:bundleName targetClass:[self class]];
+    NSURL* associateBundleURL = [[NSBundle mainBundle] URLForResource:@"Frameworks" withExtension:nil];
+    associateBundleURL = [associateBundleURL URLByAppendingPathComponent:bundleName];
+    associateBundleURL = [associateBundleURL URLByAppendingPathExtension:@"framework"];
+    NSBundle *associateBunle = [NSBundle bundleWithURL:associateBundleURL];
+    associateBundleURL = [associateBunle URLForResource:bundleName withExtension:@"bundle"];
+    NSBundle *bundle = [NSBundle bundleWithURL:associateBundleURL];
     
     return [UIImage imageNamed:imageName inBundle:bundle compatibleWithTraitCollection:nil];
 }
